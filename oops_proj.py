c8536319b2a0c06118a0691847e6aaf2b8057c3b
Sachin S <tmsachins@gmail.com>
2024-12-29 13:09:10 -0600

    covered till chatbook project

--- oops_proj.py
+++ oops_proj.py
@@ -12,15 +12,17 @@ class chatbook:
                            2. Press 2 to signin
                            3. Press 3 to write a post
                            4. Press 4 to message a friend
-                           5. Press any other key to exit""")
+                           5. Press any other key to exit
+                           
+                           ->""")
         if user_input == '1':
             self.signup()
         elif user_input == '2':
             self.signin()
         elif user_input == '3':
-            pass
+            self.my_post()
         elif user_input == '4':
-            pass
+            self.sendmsg()
         else:
             exit()
     def signup(self):
@@ -43,8 +45,26 @@ class chatbook:
                 print("Please input correct credentials..")
         print("\n")
         self.menu()
-        
-obj1=chatbook()
+
+    def my_post(self):
+        if self.loggedin==True:
+            txt = input("Enter your message here ->")
+            print(f"Following message has been posted successfully!! -> {txt}")
+        else:
+            print("Please signin first to write a post")
+        print("\n")
+        self.menu()
+    def sendmsg(self):
+        if self.loggedin==True:
+            txt = input("Please enter your message here ->")
+            frnd = input("Whom to send the msg?")            
+            print(f"Following message has been sent successfully!! -> {txt} to {frnd}")
+        else:
+            print("Please signin first to message a friend")
+        print("\n")
+        self.menu()
+
+# obj1=chatbook()
 
 
 
--- rough.py
+++ rough.py
@@ -2,13 +2,22 @@ lst = [1,2,3]
 my_str = "mlops playlist"
 my_int = 155
 
-print(type(lst))
-print(type(my_str))
-print(type(my_int))
-# lst.capitalize()
-my_str = my_str.capitalize()
+# print(type(lst))
+# print(type(my_str))
+# print(type(my_int))
+# # lst.capitalize()
+# my_str = my_str.capitalize()
 
-print(my_str)
+# print(my_str)
+
+from oops_proj import chatbook
+user1 = chatbook()
+
+
+
+
+
+
 
 # a = 'x'
 # b = 'y'
